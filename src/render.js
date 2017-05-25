d0fda4ccf984520c19f48fa1bf6a066b402209b7
Zachary Miller <zachary.john.miller@gmail.com>
2017-05-25 13:52:06 -0600

    Start minimal diffing algo with elements and props

--- src/render.js
+++ src/render.js
@@ -1,7 +1,48 @@
+const prevRenders = new Map();
+
 export default function render(domNode, element) {
-  const renderedDOMNode = turnIntoDOMNode(element);
-  domNode.innerHTML = '';
-  domNode.appendChild(renderedDOMNode);
+  if (prevRenders.has(domNode)) {
+    const queue = [[element, prevRenders.get(domNode), domNode.firstChild]];
+    while (queue.length > 0) {
+      const curTriple = queue.shift();
+      const curElement = curTriple[0];
+      const prevElement = curTriple[1];
+      const curDOMNode = curTriple[2];
+
+      if (curElement.type !== prevElement.type) {
+        // if types differ just rerender everything
+        const renderedDOMNode = turnIntoDOMNode(curElement);
+        curDOMNode.innerHTML = '';
+        curDOMNode.appendChild(renderedDOMNode);
+      } else {
+        const curElementPropNames = Object.keys(curElement.props);
+        const prevElementPropNames = Object.keys(prevElement.props);
+
+        // add or replace current props
+        curElementPropNames.forEach(propName => {
+          if (prevElementPropNames.indexOf(propName) > -1) {
+            if (curElement.props[propName] !== prevElement.props[propName]) {
+              curDOMNode[propName] = curElement.props[propName];
+            }
+          } else {
+            curDOMNode[propName] = curElement.props[propName];
+          }
+        });
+
+        // make sure all old props are deleted if not included in current props
+        prevElementPropNames.forEach(propName => {
+          if (curElementPropNames.indexOf(propName) === -1) delete curDOMNode[propName];
+        });
+      }
+    }
+  } else {
+    // if first time rendering into DOM node, don't diff
+    const renderedDOMNode = turnIntoDOMNode(element);
+    domNode.innerHTML = '';
+    domNode.appendChild(renderedDOMNode);
+  };
+
+  prevRenders.set(domNode, element);
 }
 
 function turnIntoDOMNode(element) {
@@ -9,9 +50,14 @@ function turnIntoDOMNode(element) {
 
   const domNode = document.createElement(element.type);
 
-  if (element.children) {
-    for (let i = 0; i < element.children.length; i++) {
-      const childDOMNode = turnIntoDOMNode(element.children[i]);
+  for (let prop in element.props) {
+    if (prop !== 'children') domNode[prop] = element.props[prop];
+  }
+
+  let children = element.props.children;
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const childDOMNode = turnIntoDOMNode(children[i]);
       domNode.appendChild(childDOMNode);
     }
   }
--- test/render.js
+++ test/render.js
@@ -3,31 +3,55 @@ import createElement from '../src/createElement';
 import assert from 'assert';
 import { JSDOM } from 'jsdom';
 
-const dom = new JSDOM(`<!DOCTYPE html><div id="root"><p><br></div></html>`);
-
-// following is needed to ensure render can use global document object in node context
-global.document = dom.window.document;
+beforeEach(function(){
+  const dom = new JSDOM(`<!DOCTYPE html><div id="root"><p><br></div></html>`);
+  // following is needed to ensure render can use global document object in node context
+  global.document = dom.window.document;
+});
 
 describe('render', function() {
   it('should render element', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span' });
+    render(rootNode, { type: 'span', props: {} });
     assert.equal(rootNode.firstChild.nodeName, 'SPAN');
   });
 
   it('should have cleared pre-initial-render elements', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span' });
+    render(rootNode, { type: 'span', props: {} });
     assert.equal(rootNode.children.length, 1);
+  });
+
+  it('should turn props into DOM Node properties', function() {
+    const rootNode = document.getElementById('root');
+    render(rootNode, { type: 'span', props: { className: 'base' } });
+    assert.strictEqual(rootNode.firstChild.className, 'base');
   });
 
   it('should render element with text', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span', children: 'some text' });
-    assert.strictEqual(rootNode.firstChild.innerHTML, 'some text');
-    render(rootNode, { type: 'span', children: ['some text', { type: 'p' }] });
+    render(rootNode, { type: 'span', props: { children: ['some text', { type: 'p', props: {} }] } });
     assert.strictEqual(rootNode.firstChild.childNodes.length, 2);
     assert.strictEqual(rootNode.firstChild.childNodes[0].wholeText, 'some text');
     assert.strictEqual(rootNode.firstChild.childNodes[1].nodeName, 'P');
+  });
+
+  it('should not rerender an element of same type', function() {
+    const rootNode = document.getElementById('root');
+    render(rootNode, { type: 'span', props: {} });
+    const spanNode1 = rootNode.firstChild;
+    render(rootNode, { type: 'span', props: {} });
+    const spanNode2 = rootNode.firstChild;
+    assert.strictEqual(spanNode1, spanNode2);
+  });
+
+  it('should correctly modify props when not rerendering', function() {
+    const rootNode = document.getElementById('root');
+    render(rootNode, { type: 'span', props: {a: 1, b: 2} });
+    assert.strictEqual(rootNode.firstChild.b, 2);
+    render(rootNode, { type: 'span', props: {a: 3, c: 4} });
+    assert.strictEqual(rootNode.firstChild.a, 3);
+    assert.strictEqual(rootNode.firstChild.b, undefined);
+    assert.strictEqual(rootNode.firstChild.c, 4);
   });
 });
