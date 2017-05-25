d9834a77cb34243dc564ecc4a96b873ae253dc01
Zachary Miller <zachary.john.miller@gmail.com>
2017-05-25 11:54:54 -0600

    Include props in created element

--- src/createElement.js
+++ src/createElement.js
@@ -1,6 +1,12 @@
-function createElement(type) {
+function createElement(type, props) {
+  // the following excludes children prop from props in created element
+  if (props && props.children !== undefined && props.children !== null) {
+    delete props.children;
+  }
+
   return {
     type,
+    props,
   };
 }
 
--- test/createElement.js
+++ test/createElement.js
@@ -5,4 +5,16 @@ describe('createElement', function() {
   it('should create plain JavaScript object', function() {
     assert.equal(typeof createElement('div'), 'object');
   });
+
+  it('should include props', function() {
+    const props = { className: 'base' };
+    const elem = createElement('div' , props);
+    assert.deepEqual(elem.props, { className: 'base' });
+  });
+
+  it('should exclude children prop from props', function() {
+    const props = { className: 'base', children: { type: 'span' } };
+    const elem = createElement('div' , props);
+    assert.deepEqual(elem.props, { className: 'base' });
+  });
 });
