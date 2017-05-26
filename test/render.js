c849983b0b34569f58eb83bd25c942d08107c311
Zachary Miller <zachary.john.miller@gmail.com>
2017-05-26 10:56:26 -0600

    Switch to JSX in tests

--- test/render.js
+++ test/render.js
@@ -1,3 +1,4 @@
+import React from '../src/zak-react';
 import render from '../src/render';
 import createElement from '../src/createElement';
 import assert from 'assert';
@@ -12,25 +13,25 @@ beforeEach(function(){
 describe('render', function() {
   it('should render element', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span', props: {} });
+    render(rootNode, <span></span>);
     assert.equal(rootNode.firstChild.nodeName, 'SPAN');
   });
 
   it('should have cleared pre-initial-render elements', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span', props: {} });
+    render(rootNode, <span></span>);
     assert.equal(rootNode.children.length, 1);
   });
 
   it('should turn props into DOM Node properties', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span', props: { className: 'base' } });
+    render(rootNode, <span className="base"></span>);
     assert.strictEqual(rootNode.firstChild.className, 'base');
   });
 
   it('should render element with text', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span', props: { children: ['some text', { type: 'p', props: {} }] } });
+    render(rootNode, <span>some text<p/></span>);
     assert.strictEqual(rootNode.firstChild.childNodes.length, 2);
     assert.strictEqual(rootNode.firstChild.childNodes[0].wholeText, 'some text');
     assert.strictEqual(rootNode.firstChild.childNodes[1].nodeName, 'P');
@@ -38,20 +39,27 @@ describe('render', function() {
 
   it('should not rerender an element of same type', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span', props: {} });
+    render(rootNode, <span></span>);
     const spanNode1 = rootNode.firstChild;
-    render(rootNode, { type: 'span', props: {} });
+    render(rootNode, <span></span>);
     const spanNode2 = rootNode.firstChild;
     assert.strictEqual(spanNode1, spanNode2);
   });
 
   it('should correctly modify props when not rerendering', function() {
     const rootNode = document.getElementById('root');
-    render(rootNode, { type: 'span', props: {a: 1, b: 2} });
+    render(rootNode, <span a={1} b={2}></span>);
     assert.strictEqual(rootNode.firstChild.b, 2);
-    render(rootNode, { type: 'span', props: {a: 3, c: 4} });
+    render(rootNode, <span a={3} c={4}></span>);
     assert.strictEqual(rootNode.firstChild.a, 3);
     assert.strictEqual(rootNode.firstChild.b, undefined);
     assert.strictEqual(rootNode.firstChild.c, 4);
+  });
+
+  it('should render children', function() {
+    const rootNode = document.getElementById('root');
+    render(rootNode, <span><p /></span>);
+    assert.strictEqual(rootNode.firstChild.nodeName, 'SPAN');
+    assert.strictEqual(rootNode.firstChild.firstChild.nodeName, 'P');
   });
 });
